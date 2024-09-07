10ffdef45820e60867bb65bc19932aee28d333de
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-09-07 16:21:02 +0100

    ajouter deux séction dans la page Home , une de définition des espaces et une de l inspiration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatCardContent, MatCardImage, MatCardModule} from "@angular/material/card";
+import { MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatCardContent,
+    MatCardImage,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
