5cb190a12f3afb73e507e08a83f626d15db6eb73
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-09-14 22:27:40 +0100

    terminer l' affichage d' interface event dont j' ai afficher les evenenements disponible avec posibilité de réservé dans un evenement

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,15 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FooterComponent} from "../footer/footer.component";
+import {DatePipe, NgForOf} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import {MatCard, MatCardActions, MatCardContent, MatCardImage} from "@angular/material/card";
+import {Event} from "../models/event";
+import {EventService} from "../services/event.service";
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-events',
   standalone: true,
-    imports: [
-        FooterComponent
-    ],
+  imports: [
+    FooterComponent,
+    DatePipe,
+    MatButton,
+    MatCard,
+    MatCardActions,
+    MatCardContent,
+    MatCardImage,
+    NgForOf,
+    MatPaginator
+  ],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit{
+  events: Event[]=[];
+  pagedEvents: Event[] = [];
+  pageSize = 6;
+  pageIndex = 0;
 
+  constructor(
+    private eventService: EventService,
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.getEvents();
+  }
+
+  getEvents(){
+    this.eventService.getAllEvents().subscribe(data => {
+      this.events = data;
+      this.setPagedEvents();
+    });
+  }
+
+
+
+  onPageChange(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    this.setPagedEvents();
+  }
+
+  setPagedEvents() {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedEvents = this.events.slice(startIndex, endIndex);
+  }
 }
