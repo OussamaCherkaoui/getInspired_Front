62880e22759c0591c128703b3a962ee084a0c7b2
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-10-13 18:05:27 +0100

    amélioration du page unauthorized

--- src/app/unauthorized/unauthorized.component.ts
+++ src/app/unauthorized/unauthorized.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-unauthorized',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardTitle,
+    MatCard,
+    MatCardHeader,
+    MatIcon,
+    MatCardContent,
+    MatButton
+  ],
   templateUrl: './unauthorized.component.html',
   styleUrl: './unauthorized.component.css'
 })
