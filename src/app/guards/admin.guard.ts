0dea2ab2cbf50075fdfcd6d546e1fbd32463d307
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-10-13 18:05:33 +0100

    amélioration du page unauthorized

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -6,12 +6,20 @@ import {Role} from "../models/role";
 
 export const adminGuard: CanActivateFn = (route, state) => {
   const authService = inject(DecodejwtService);
+  const userService = inject(UserService);
   const router = inject(Router);
 
-  if (authService.getRoleFromToken()===Role.ADMIN) {
-    return true;
-  } else {
-    router.navigate(['/unauthorized']);
-    return false;
-  }
+  userService.isLoggedIn.subscribe((loggedIn: boolean) => {
+    if (!loggedIn) {
+      router.navigate(['/unauthorized']);
+      return false;
+    }
+    else if (authService.getRoleFromToken()===Role.ADMIN) {
+      return true;
+    } else {
+      router.navigate(['/unauthorized']);
+      return false;
+    }
+  });
+  return true;
 };
